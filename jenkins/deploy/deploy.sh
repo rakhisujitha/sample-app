be0f5f644551c78fa8775907dff5e29461694987
Ubuntu <ubuntu@ip-10-0-8-40.us-east-2.compute.internal>
2023-10-21 06:58:53 +0000

    deployed

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,7 +7,7 @@ echo "maven-project" > /tmp/.auth
 echo "$BUILD_TAG" >> /tmp/.auth
 echo "$PASS" >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth prod-user@3.16.70.110:/tmp/.auth
-scp -i /opt/prod ./jenkins/deploy/publish prod-user@3.16.70.110:/tmp/publish
-ssh -i /opt/prod  prod-user@3.16.70.110 "/tmp/publish"
+scp -i /opt/prod /tmp/.auth prod-user@18.224.44.36:/tmp/.auth
+scp -i /opt/prod ./jenkins/deploy/publish prod-user@18.224.44.36:/tmp/publish
+ssh -i /opt/prod  prod-user@18.224.44.36 "/tmp/publish"
 
