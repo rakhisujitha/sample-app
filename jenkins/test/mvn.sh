0406505c0cdb8899372d728c80e1a5e5f94b071c
Ubuntu <ubuntu@ip-10-0-8-40.us-east-2.compute.internal>
2023-10-21 05:56:53 +0000

    modified pwd

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,5 +4,5 @@ echo "****************************************"
 echo "******BUILDING THE JAR *****************"
 echo "****************************************"
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
-docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.8.6-openjdk-11-slim "$@"
+
+docker run --rm -v $PWD/java-app:/app -v /root/.m2:/root/.m2/ -w /app maven:3.8.6-openjdk-11-slim "$@"
